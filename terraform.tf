50ceaf5b03e6ad5d26a0a8ae5f25945e820f7731
Christina Jeong <christina.jeong@coxautoinc.com>
2024-03-24 23:25:42 -0500

    feat: version controll workflow demo

---
+++ terraform.tf
@@ -0,0 +1,22 @@
+//Note: Only for CLI-driven workflow
+# terraform {
+
+#   cloud {
+#     organization = "acme-corp-poc"
+
+#     workspaces {
+#       name = "demo"
+#     }
+
+#   }
+
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.31.0"
+#     }
+#   }
+
+#   required_version = "~> 1.2"
+# }
+
